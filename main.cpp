22d5d7acf9152e051effa74f5fb61e62435443b6
junios <junios@naver.com>
2023-04-14 16:29:26 +0900

    SDL 추가

--- main.cpp
+++ main.cpp
@@ -2,7 +2,24 @@
 #include "MyGameEngine.h"
 #include "SDL.h"
 
+#pragma comment(lib, "SDL2.lib")
+#pragma comment(lib, "SDL2main.lib")
+
 using namespace std;
+
+int SDL_main(int argc, char* argv[])
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	SDL_Window* MyWindow = SDL_CreateWindow("Game", 100, 100, 800, 600, SDL_WINDOW_VULKAN);
+	SDL_Renderer* MyRenderer = SDL_CreateRenderer(MyWindow, -1, SDL_RENDERER_TARGETTEXTURE | SDL_RENDERER_ACCELERATED);
+	while (1);
+
+	SDL_DestroyRenderer(MyRenderer);
+	SDL_DestroyWindow(MyWindow);
+	SDL_Quit();
+	return 0;
+}
 
 int main()
 { 
